bdaca3f9e14810fba768ed054988a5198f7d5aba
benny <bennytajfar@gmail.com>
2020-09-17 16:00:49 -0500

    Updates

---
+++ precache-manifest.b2750d77f78bd8070318a0d469df6bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6dd71fcbea32a08a2f0edd3b7f57ce",
+    "url": "/bennyapp/index.html"
+  },
+  {
+    "revision": "132c49a80d0da001b3c1",
+    "url": "/bennyapp/static/css/main.5aa2e881.chunk.css"
+  },
+  {
+    "revision": "98c48ee35cab1f1ede15",
+    "url": "/bennyapp/static/js/2.52ad32b4.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/bennyapp/static/js/2.52ad32b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132c49a80d0da001b3c1",
+    "url": "/bennyapp/static/js/main.76561f45.chunk.js"
+  },
+  {
+    "revision": "cdaa3091ec2fd1b4c75b",
+    "url": "/bennyapp/static/js/runtime-main.dd0552d4.js"
+  }
+]);
